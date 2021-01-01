0760584c5e7e7576b3f611bd194d50de866029ff
enjoycoding <hasaraha@yahoo.de>
2021-01-01 15:17:33 +0100

    update POM to  Parent snapshot

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -0,0 +1,45 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Service
+public class BeerServiceImpl implements BeerService{
+
+    public static final String  BEER_PATH_V1 = "/api/v1/beer/";
+    public final static  String BEER_UPC_PATH_V1 ="/api/v1/beerUpc";
+    private RestTemplate restTemplate;
+
+    private String beerServiceHost;
+
+    @Autowired
+    private BeerService beerService;
+
+    public BeerServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID uuid) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class));
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerByUpc(String upc) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_UPC_PATH_V1 + upc, BeerDto.class));
+    }
+
+    public void setBeerServiceHost(String beerServiceHost) {
+        this.beerServiceHost = beerServiceHost;
+    }
+
+}
